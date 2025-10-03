4e7be5ac0f2de326f3c42ac870e3a736d84af77b
Rafael <rafael.oliveira.silva09@escola.pr.gov.br>
2025-10-03 11:07:47 -0300

    nnn

--- js/principal.js
+++ js/principal.js
@@ -2,7 +2,9 @@ var titulo = document.querySelector("h1");
 console.log(titulo);
 console.log(titulo.textContent);
 titulo.textContent = ("Aparecida Nutricionista");
+
 var paciente = document.querySelector("#primeiro-paciente");
-console.log(paciente);
 var tdPeso = paciente.querySelector(".info-peso");
-console.log(peso);
+var peso= tdPeso.textContent;
+console.log(paciente);
+console.log(tdPeso);
