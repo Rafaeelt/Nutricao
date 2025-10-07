906b61f2d7040a9d7ea33c109e2294a2c60faec0
Rafael <rafael.oliveira.silva09@escola.pr.gov.br>
2025-10-07 09:18:51 -0300

    5

--- js/principal.js
+++ js/principal.js
@@ -9,7 +9,6 @@ console.log(nome);
 
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
-
 console.log(peso);
 
 var tdAltura = document.querySelector(".info-altura");
