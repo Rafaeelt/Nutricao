a108ef163908d39020e655500818e6df4222efb3
Rafael <rafael.oliveira.silva09@escola.pr.gov.br>
2025-10-10 11:00:05 -0300

    2

--- js/principal.js
+++ js/principal.js
@@ -25,13 +25,13 @@ var alturaEhValida = true;
 if (peso < 0 || peso > 1000 {
     console.log("Peso inválido");
     pesoEhValido = false;
-
+    tdImc.textContent = "Peso inválido";
 }
 
 if(altura < 0 || altura > 3.00 {
     console.log("Altura inválida!");
     alturaEhValida = true;
-
+    tdImc.textContent = "Altura inválida";
 }
 
 
