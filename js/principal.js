7067a124a68b32b92f165f2a646858946520bea9
Rafael <rafael.oliveira.silva09@escola.pr.gov.br>
2025-10-21 09:48:19 -0300

    55

--- js/principal.js
+++ js/principal.js
@@ -4,43 +4,41 @@ console.log(titulo.textContent);
 titulo.textContent = ("Aparecida Nutricionista");
 
 var pacientes = document.querySelectorAll(".paciente");
-
     for (var i = 0; 1  < pacientes.length; i++) {
 
     var pacientes = [i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = document.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc")
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+    }
+
+    if(altura <= 0 || altura >= 3.00 ){
+        console.log("Altura inválida!");
+        alturaEhValida = true;
+        tdImc.textContent = "Altura inválida";
+    }
+
+
+    if (alturaEhValida && pesoEhValido){
+        var imc=peso / (altura*altura) ;
+        tdImc.textCotnent=imc;
+    }
 }
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-console.log(peso);
 
-var tdAltura = document.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-console.log(altura);
-
-var tdImc = paciente.querySelector(".info-imc")
-console.log(imc);
-
-var pesoEhValido = true;
-var alturaEhValida = true;
-
-if (peso < 0 || peso > 1000) {
-    console.log("Peso inválido");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido";
-}
-
-if(altura < 0 || altura > 3.00 ){
-    console.log("Altura inválida!");
-    alturaEhValida = true;
-    tdImc.textContent = "Altura inválida";
-}
-
-
-if (alturaEhValida && pesoEhValido){
-    var imc=peso / (altura*altura) ;
-    tdImc.textCotnent=imc;
-}
 
 
 
